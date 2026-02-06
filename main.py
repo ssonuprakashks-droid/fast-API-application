74ef5d20630b1e60c236aed77c854825a783d9fa
sonu prakash k.s <ssonuprakashks@gmail.com>
2026-02-06 10:21:34 +0530

    fast appi

--- main.py
+++ main.py
@@ -1,5 +1,46 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import List
 app = FastAPI()
+class student(BaseModel):
+    name: str
+    email:str
+    age: int
+    roll_number:str 
+    department:str
+class studentResponse(BaseModel):
+    id:int
+    name:str
+    email:str
+    age: int
+    roll_number:str 
+    department:str
+   
+
+   
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+
+def create_student(student: student):
+    return student
+def read_student(id: int):
+    return studentResponse(id=id, **student.dict())  
+def update_student(student_id: int, student: student):
+    return studentResponse(id=student_id, **student.dict())   
+def delete_student(student_id: int):
+    return studentResponse(id=student_id, **student.dict())        
+@app.post("/student")
+def create_student(student: student):
+    return create_student(student)
+@app.get("/student/{id}")
+def read_student(id: int):
+    return read_student(id)
+@app.put("/student/{student_id}")
+def update_student(student_id: int, student: student):
+    return update_student(student_id, student)
+@app.delete("/student/{student_id}")
+def delete_student(student_id: int):
+    return delete_student(student_id)    
+
